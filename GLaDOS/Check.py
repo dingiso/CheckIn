4f97adfbb4e2c0a0b100c84fc8c49ea8109fcad0
Ruibo Lu <reaperlu@hust.edu.cn>
2023-08-11 15:24:46 +0800

    Fix token in payload

--- GLaDOS/Check.py
+++ GLaDOS/Check.py
@@ -9,7 +9,7 @@ def CheckIn(cookie):
     origin = "https://glados.rocks"
     useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
     payload = {
-        'token': 'glados.network'
+        'token': 'glados.one'
     }
     checkin = post(
         url,
